7b35312f2b7b855bd222fe0f6e73923a7a398ff7
I Gede Surya Rahayuda <surya.rahayuda@gmail.com>
2023-04-19 23:16:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/volleyselect/MainActivity.java
@@ -0,0 +1,175 @@
+package com.example.volleyselect;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    public static final String URLSelect = "https://websensordata.000webhostapp.com/android/select.php";
+    public static final String URLDelete = "https://websensordata.000webhostapp.com/android/delete.php";
+    //public static final String URLSelect = "http://192.168.1.5:8080/kampus/android/select.php";
+    //public static final String URLDelete = "http://192.168.1.5:8080/kampus/android/delete.php";
+
+    ListView list;
+    SwipeRefreshLayout swipe;
+    List<Data> itemList = new ArrayList<Data>();
+    MhsAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        swipe = (SwipeRefreshLayout) findViewById(R.id.swipe);
+        list = (ListView) findViewById(R.id.list);
+
+        adapter = new MhsAdapter(MainActivity.this, itemList);
+        list.setAdapter(adapter);
+
+        swipe.setOnRefreshListener(this);
+
+        swipe.post(new Runnable() {
+                       @Override
+                       public void run() {
+                           swipe.setRefreshing(true);
+                           itemList.clear();
+                           adapter.notifyDataSetChanged();
+                           callVolley();
+                       }
+                   }
+        );
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                final String idx = itemList.get(position).getId();
+                final CharSequence[] pilihanAksi = {"Delete", "Edit"};
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setItems(pilihanAksi, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        switch (which){
+                            case 0:
+                                hapusData(idx);
+                                break;
+                            case 1:
+                                break;
+                        }
+                    }
+                }).show();
+                return false;
+            }
+        });
+    }
+
+    public void hapusData(String id){
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URLDelete,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(MainActivity.this, response, Toast.LENGTH_LONG).show();
+                        callVolley();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Gagal Koneksi Server", Toast.LENGTH_LONG).show();
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+
+                params.put("id", id);
+                return params;
+            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(stringRequest);
+    }
+
+    @Override
+    public void onRefresh() {
+        //  itemList.clear();
+        //  adapter.notifyDataSetChanged();
+        callVolley();
+    }
+
+    private void callVolley() {
+        itemList.clear();
+        adapter.notifyDataSetChanged();
+        swipe.setRefreshing(true);
+
+        // membuat request JSON
+        JsonArrayRequest jArr = new JsonArrayRequest(URLSelect, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                // Parsing json
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject obj = response.getJSONObject(i);
+
+                        Data item = new Data();
+
+                        item.setId(obj.getString("id"));
+                        item.setNim(obj.getString("nim"));
+                        item.setNama(obj.getString("nama"));
+                        item.setAlamat(obj.getString("alamat"));
+
+                        // menambah item ke array
+                        itemList.add(item);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                // notifikasi adanya perubahan data pada adapter
+                adapter.notifyDataSetChanged();
+
+                swipe.setRefreshing(false);
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "gagal koneksi ke server, cek setingan koneksi anda", Toast.LENGTH_LONG).show();
+                swipe.setRefreshing(false);
+            }
+        });
+
+        // menambah request ke request queue
+        RequestQueue mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        mRequestQueue.add(jArr);
+
+    }
+
+}
